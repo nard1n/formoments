7ebac1a9ad1bd679b8269ef21abb1197471e93fb
Nardin Lachowski <nardin@live.com>
2021-02-24 11:54:44 -0500

    initial file structure

---
+++ server/index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+// the above is new syntax instead of writing const express = require('express');
+// one additional thing to do is go to package.json and add: "type":"module"
+
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
+app.use(cors());
+
+//cloud atlast version of mongodb: https://www.mongodb.com/cloud/atlas
+const CONNECTION_URL = 'mongodb+srv://javascriptmastery:javascriptmastery123@cluster0.yhw5f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+.then (() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+.catch ((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
