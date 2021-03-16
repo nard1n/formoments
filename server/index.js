6b4e64691b4763b988810e5209bdaa3c914a8b6a
Nardin Lachowski <nardin@live.com>
2021-03-15 21:52:17 -0400

    moved the posts routes placement to be after cors

--- server/index.js
+++ server/index.js
@@ -2,19 +2,16 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+
 import postRoutes from './routes/posts.js';
 
-// the above is new syntax instead of writing const express = require('express');
-// one additional thing to do is go to package.json and add: "type":"module"
-
-
 const app = express();
-
-app.use('/posts', postRoutes);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
 app.use(cors());
+
+app.use('/posts', postRoutes);
 
 //cloud atlast version of mongodb: https://www.mongodb.com/cloud/atlas
 const CONNECTION_URL = 'mongodb+srv://javascriptmastery:javascriptmastery123@cluster0.yhw5f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
