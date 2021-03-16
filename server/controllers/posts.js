daa7c5da5e1a3dd5d974f2853e8fdcc77b466d5b
Nardin Lachowski <nardin@live.com>
2021-03-15 21:53:33 -0400

    corrected spacing

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -6,7 +6,7 @@ export const getPosts = async (req,res) => {
 
         res.status(200).json(postMessages);
     } catch (error) {
-        res.status(404).json({ message: error.message});
+        res.status(404).json({ message: error.message });
     }
 }
 
@@ -22,7 +22,7 @@ export const createPost = async (req,res) => {
 
     } catch (error) {
         
-        res.status(409).json({message: error.message});
+        res.status(409).json({ message: error.message });
 
     }
 }
